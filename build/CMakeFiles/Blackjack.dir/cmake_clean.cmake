ef74f22a31506d221cea966d9f7f127590f7c085
RdVL <rauldel.valle.lima@gmail.com>
2022-10-20 13:45:11 +0200

    CMake

---
+++ build/CMakeFiles/Blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "Blackjack.exe"
+  "Blackjack.exe.manifest"
+  "Blackjack.pdb"
+  "CMakeFiles/Blackjack.dir/src/deck.cpp.obj"
+  "CMakeFiles/Blackjack.dir/src/deck.cpp.obj.d"
+  "CMakeFiles/Blackjack.dir/src/main.cpp.obj"
+  "CMakeFiles/Blackjack.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Blackjack.dir/src/player.cpp.obj"
+  "CMakeFiles/Blackjack.dir/src/player.cpp.obj.d"
+  "libBlackjack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
