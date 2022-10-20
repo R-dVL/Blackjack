1344f093d3ac5a7e3e0ba83c0d67c56cd12f4f1f
RdVL <rauldel.valle.lima@gmail.com>
2022-10-20 13:55:40 +0200

    Eliminated CMake Test

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/stc/Documents/GitHub/Blackjack
-# Build directory: C:/Users/stc/Documents/GitHub/Blackjack/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
